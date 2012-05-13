5a52af69d26e14ea07248758571cd1736932ede1
Carlos Crespo <carlos.crespog@gmail.com>
2012-05-13 02:33:17 +0200

    First Commit

---
+++ src/es/upm/dit/MemoryCoconutActivity.java
@@ -0,0 +1,13 @@
+package es.upm.dit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MemoryCoconutActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
