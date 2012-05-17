6b1978c75d8debae3af172122ea6b276c247616c
Enrique Garcia <garcianavalon@gmail.com>
2012-05-17 15:41:41 +0200

    Batido de Coco con imagen

--- gen/es/upm/dit/R.java
+++ gen/es/upm/dit/R.java
@@ -15,17 +15,19 @@ public final class R {
         public static final int letras=0x7f050001;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int cocomalo=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int body=0x7f070001;
-        public static final int confirm=0x7f070005;
-        public static final int dateDisplay=0x7f070003;
-        public static final int email=0x7f070002;
-        public static final int pickDate=0x7f070004;
-        public static final int text1=0x7f070006;
-        public static final int text2=0x7f070007;
-        public static final int title=0x7f070000;
+        public static final int body=0x7f070002;
+        public static final int cocoBtn=0x7f070000;
+        public static final int confirm=0x7f070006;
+        public static final int dateDisplay=0x7f070004;
+        public static final int email=0x7f070003;
+        public static final int pickDate=0x7f070005;
+        public static final int text1=0x7f070007;
+        public static final int text2=0x7f070008;
+        public static final int title=0x7f070001;
     }
     public static final class layout {
         public static final int acercade_view=0x7f030000;
@@ -39,6 +41,7 @@ public final class R {
         public static final int alert=0x7f04000a;
         public static final int app_name=0x7f040000;
         public static final int body=0x7f040005;
+        public static final int button_batido=0x7f040011;
         public static final int confirm=0x7f040006;
         public static final int content_acercade=0x7f04000c;
         public static final int content_ayuda=0x7f04000f;
@@ -55,6 +58,7 @@ public final class R {
         public static final int title_ayuda=0x7f040010;
     }
     public static final class style {
+        public static final int BatidoCoco=0x7f060001;
         public static final int CocoTheme=0x7f060000;
     }
 }
--- src/es/upm/dit/AyudaActivity.java
+++ src/es/upm/dit/AyudaActivity.java
@@ -1,15 +1,30 @@
 package es.upm.dit;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class AyudaActivity extends Activity {
-
+public class AyudaActivity extends Activity implements View.OnClickListener {
+	private Button cocoBtn;
+	private Vibrator vibrator;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ayuda_layout); //Cargamos la vista
 		setTitle(R.string.title_ayuda);
+		cocoBtn = (Button)findViewById(R.id.cocoBtn);
+		cocoBtn.setOnClickListener(this); //this referencia a esta clase, necesita implementar la interfaz View.OnClickListener para k funcione (le obliga a tner metodo onClick)
+		vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+	}
+	public void onClick(View view) {
+		vibrator.vibrate(500); //activamos vibracion
+		ImageView imageView = new ImageView(this); //sacamos la imagen por pantalla
+		imageView.setImageResource(R.drawable.cocomalo);
+		setContentView(imageView);
 	}
 	
 
