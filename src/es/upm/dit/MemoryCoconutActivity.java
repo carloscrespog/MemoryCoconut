f6db821b4f3173188d429d3c74b5d484446d6ec3
Carlos Crespo <carlos.crespog@gmail.com>
2012-05-17 03:45:43 +0200

    NotesDbAdapter

--- src/es/upm/dit/MemoryCoconutActivity.java
+++ src/es/upm/dit/MemoryCoconutActivity.java
@@ -1,9 +1,9 @@
 package es.upm.dit;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MemoryCoconutActivity extends Activity {
+public class MemoryCoconutActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
