556b6cad308c9b9e1ad832f38ce2ce1f04ef4d1b
Enrique Garcia <garcianavalon@gmail.com>
2012-05-29 19:49:47 +0200

    layouts,ayuda y acerca de terminados por mi parte

--- gen/es/upm/dit/R.java
+++ gen/es/upm/dit/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class color {
         public static final int fondo=0x7f050000;
         public static final int letras=0x7f050001;
+        public static final int titulos=0x7f050002;
     }
     public static final class drawable {
         public static final int cocomalo=0x7f020000;
@@ -49,28 +50,32 @@ public final class R {
         public static final int alert=0x7f04000b;
         public static final int app_name=0x7f040000;
         public static final int body=0x7f040005;
-        public static final int button_batido=0x7f040013;
-        public static final int button_mail=0x7f040014;
+        public static final int button_batido=0x7f040016;
+        public static final int button_mail=0x7f040017;
         public static final int confirm=0x7f040006;
-        public static final int content_acercade=0x7f04000d;
-        public static final int content_ayuda1=0x7f040010;
-        public static final int content_ayuda2=0x7f040011;
+        public static final int content_acercade1=0x7f04000d;
+        public static final int content_acercade2=0x7f04000e;
+        public static final int content_ayuda1=0x7f040011;
+        public static final int content_ayuda2=0x7f040012;
+        public static final int content_ayuda3=0x7f040013;
+        public static final int content_ayuda4=0x7f040014;
         public static final int edit_note=0x7f040007;
         public static final int email=0x7f040009;
         public static final int hello=0x7f04000a;
         public static final int menu_acercade=0x7f04000c;
-        public static final int menu_ayuda=0x7f04000f;
+        public static final int menu_ayuda=0x7f040010;
         public static final int menu_delete=0x7f040003;
         public static final int menu_insert=0x7f040002;
         public static final int no_notes=0x7f040001;
         public static final int title=0x7f040004;
-        public static final int title_acercade=0x7f04000e;
-        public static final int title_ayuda=0x7f040012;
+        public static final int title_acercade=0x7f04000f;
+        public static final int title_ayuda=0x7f040015;
         public static final int view_note=0x7f040008;
     }
     public static final class style {
-        public static final int BatidoCoco=0x7f060001;
+        public static final int BatidoCoco=0x7f060002;
         public static final int CocoTheme=0x7f060000;
-        public static final int titulos=0x7f060002;
+        public static final int CocoTheme2=0x7f060001;
+        public static final int titulos=0x7f060003;
     }
 }
--- src/es/upm/dit/AyudaActivity.java
+++ src/es/upm/dit/AyudaActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 public class AyudaActivity extends Activity implements View.OnClickListener {
 	private Button cocoBtn;
 	private Vibrator vibrator;
+	private ImageView imageView;
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +21,17 @@ public class AyudaActivity extends Activity implements View.OnClickListener {
 		cocoBtn = (Button)findViewById(R.id.cocoBtn);
 		cocoBtn.setOnClickListener(this); //this referencia a esta clase, necesita implementar la interfaz View.OnClickListener para k funcione (le obliga a tner metodo onClick)
 		vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+		imageView = new ImageView(this);
+		imageView.setImageResource(R.drawable.cocomalo);
 	}
 	public void onClick(View view) {
+		if(view!=imageView){
 		vibrator.vibrate(500); //activamos vibracion
-		ImageView imageView = new ImageView(this); //sacamos la imagen por pantalla
-		imageView.setImageResource(R.drawable.cocomalo);
-		setContentView(imageView);
-	}
-	
-	
+		setContentView(imageView); //sacamos la imagen por pantalla
+		imageView.setOnClickListener(this);
+		}
+		else
+		setContentView(R.layout.ayuda_layout); //Cargamos la vista original
+	}	
 
 }
